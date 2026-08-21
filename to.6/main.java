37101d308f395b2fb8b3fe040dd7525a2456c78b
malikghazaly9-ai <malikghazaly9@gmail.com>
2026-08-21 13:55:20 +0000

    to.6

---
+++ to.6/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        lecturer l = new lecturer("L101", "Dr medhat","OOP", "Faculty of Information Technology");
+        l.displayInfo();
+        l.displaySubject();
+        l.displayDepartment();
+    }
+}
