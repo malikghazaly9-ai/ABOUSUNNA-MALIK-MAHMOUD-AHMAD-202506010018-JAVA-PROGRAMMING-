45a7d378d88039abb710e4eb0e10b9975221c6d8
malikghazaly9-ai <malikghazaly9@gmail.com>
2026-08-21 13:19:03 +0000

    tutorial 2

---
+++ to.2/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        //coding starts here;
+
+        Student s1 = new Student("malik",23,4.0);
+        s1.displayInfo();
+        s1.study();
+        s1.takeExam();
